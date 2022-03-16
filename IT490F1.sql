266ae986174643f938baf9645d2c36e8d98a6b61
Gadgetzzz <94268161+Gadgetzzz@users.noreply.github.com>
2022-03-16 15:09:09 -0400

    Finished user table and created more full bracket

--- IT490F1.sql
+++ IT490F1.sql
@@ -1,6 +1,10 @@
 CREATE TABLE `brackets` (
   `bracketID` smallint NOT NULL AUTO_INCREMENT,
-  'bracketName' varchar(40) NOT NULL,
+  `bracketName` varchar(40) NOT NULL,
+  `player1ID` int NOT NULL,
+  `player1Driver` varchar(40) NOT NULL,
+  `player1PitCrew` varchar(40) NOT NULL,
+  `player1Score` int NOT NULL, 
 ) DEFAULT CHARSET=utf8mb4;
 
 
@@ -9,12 +13,13 @@ CREATE TABLE `users` (
   `username` varchar(40) NOT NULL,
   `hashPass` varchar(40) NOT NULL,
   'email' varchar (40) NOT NULL,
+  'isNotif' tinyint NOT NULL,
 ) DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE 'races' (
-  'raceName' varchar(40) NOT NULL,
-  'raceLocation' varchar(50) NOT NULL,
-  'raceDT' datetime NOT NULL,
+CREATE TABLE `races` (
+  `raceName` varchar(40) NOT NULL,
+  `raceLocation` varchar(50) NOT NULL,
+  `raceDT` datetime NOT NULL,
 ) DEFAULT CHARSET=utf8mb4
 
 
@@ -24,6 +29,9 @@ ALTER TABLE `brackets`
 
 ALTER TABLE `users`
   ADD PRIMARY KEY (`userID`);
+  
+ALTER TABLE `races`
+  ADD PRIMARY KEY (`raceName`)
 
 
 
