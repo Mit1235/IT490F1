cee215fa4a992129d19c55ef097dd23b2fbd3f5f
sarahthomas13 <st665@.njit.edu>
2022-03-16 18:08:40 -0400

    errorlog.php

--- errorlog.php
+++ errorlog.php
@@ -1,47 +1,85 @@
+#!/usr/bin/php
+
 <?php
 
-display_errors = on
-ini_set('display_errors', 1);
-ini_set('log_errors', 1);
-ini_set('error_log', dirname(__FILE__) . '~git/IT490F1/IT490F1/errorlog.txt');
-php_value error_log log/errorlog.txt
+//incldue('includes.php');
 
 error_reporting(E_ALL & ~E_WARNING & ~E_CORE_WARNING & ~E_COMPILE_WARNING & ~E_USER_WARNING);
+ini_set('display_errors', 'Off');
+ini_set('log_errors', 'On');
+ini_set('error_log',"/home/parallels/git/IT490F1/IT490F1/errorlog.txt";
 
-php_flag display_errors on 
+$file = file_get_contents ("/home/parallels/git/IT490F1/IT490F1/errorlog.txt");
+$errorArray = [];
+$request = array();
+$request ['type'] = "parallels";
+$request['error_string'] =$file;
 
-
-
-
-?>
-
-
-
-<?php
-
-catch (Exception $e) {
-	$datetime = new DateTime();
-	$datetime->setTimezone(new DateTimeZone('EST'));
-	$logEntry = $datetime->format('Y/m/d H:i:s') . ‘ ‘ . $e;
-	
-	// log to default error_log destination
-	error_log($logEntry);
-	
-	//Email or notice someone
-	}
-
-error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_WARNING & ~E_ERROR);
-
-// Report simple running errors
-// Report all PHP errors
-error_reporting(E_ALL);
-
-// Report all PHP errors
-error_reporting(-1);
-
-// Same as error_reporting(E_ALL);
-ini_set('error_reporting', E_ALL);
+$returnedValue = createClient($request);
 
 
+file_put_contents ("/home/parallels/git/IT490F1/IT490F1/errorlog.txt", $returnedValue, FILE_APPEND);
 
 }?>
+
+
+
+
+
+<BS><?php
+
+//    include("../php/functions.php");
+//
+//    logAndSendErrors();
+
+
+    //  Error logging
+    error_reporting(E_ALL);
+
+    ini_set('display_errors', 'On');
+    ini_set('log_errors', 'On');
+    //ini_set('error_log', dirname(__FILE__). '/../logging/log.txt');
+
+    //  Requireing required files
+    require_once('/home/parallels/git/IT490F1/IT490F1/path.inc');
+    require_once('/home/parallels/git/IT490F1/IT490F1/get_host_info.inc');
+    require_once('/home/parallels/git/IT490F1/IT490F1/rabbitMQLib.inc');
+    //require_once('../php/rabbitMQClient.php');
+
+    $file = fopen("/home/parallels/git/IT490F1/IT490F1/errorlog.txt","r");
+    $errorArray = [];
+    while(! feof($file)){
+        array_push($errorArray, fgets($file));
+    }
+
+    fclose($file);
+
+    $request = array();
+    $request['type'] = "dmz";  
+    $request['error_string'] = $errorArray;
+    $returnedValue = createClientForRmq($request);
+
+    $fp = fopen("/home/parallels/git/IT490F1/IT490F1/errorlog.txt", "a");
+    for($i = 0; $i < count($errorArray); $i++){
+        fwrite($fp, $errorArray[$i]);
+    }
+
+    file_put_contents("/home/parallels/git/IT490F1/IT490F1/errorlog.txt", "");
+
+    function createClientForRmq($request){
+            $client = new rabbitMQClient("/home/parallels/git/IT490F1/IT490F1/testRabbitMQ.ini", "testServer");
+
+            if(isset($argv[1])){
+                $msg = $argv[1];
+            }
+            else{
+                $msg = "client";
+            }
+
+
+            $response = $client->send_request($request);
+
+            return $response;
+        }
+
+?> 
