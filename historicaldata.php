84e33c397e0bacfd19d725c5aaa81bbde7f908ae
Mit1235 <Mitpatelmat@gmail.com>
2022-03-20 11:56:16 -0400

    history data both parts

--- historicaldata.php
+++ historicaldata.php
@@ -1,5 +1,13 @@
 <?php
-$curl = curl_init();
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+
+function history($msg)
+{
+	
+	$curl = curl_init();
 $test = "last";
 curl_setopt_array($curl, array(
 	CURLOPT_URL => "http://ergast.com/api/f1/current/{$test}/results",
@@ -20,13 +28,9 @@ curl_close($curl);
 if ($err) {
     echo "cURL Error #:" . $err;
 } else {
-	echo $response;
+	//echo $response;
 	}	
 	
-require_once('path.inc');
-require_once('get_host_info.inc');
-require_once('rabbitMQLib.inc');
-
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 if (isset($argv[1]))
 {
@@ -37,7 +41,8 @@ else
   $msg = $response;
 }
 
-//$request = array();
+$request = array();
+$request['type'] = "history";
 $request['message'] = $msg;
 $response = $client->send_request($request);
 //$response = $client->publish($request);
@@ -47,7 +52,31 @@ print_r($response);
 echo "\n\n";
 
 echo $argv[0]." END".PHP_EOL;
+}
 
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "history":
+      return history($request['msg']);
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}
+
+
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+
+echo "testRabbitMQServer BEGIN".PHP_EOL;
+$server->process_requests('requestProcessor');
+echo "testRabbitMQServer END".PHP_EOL;
+exit();
 
 	
 
