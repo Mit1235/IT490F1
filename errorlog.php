d8259d0c807cd12dc65e8d34b203e4654647436b
Mit1235 <Mitpatelmat@gmail.com>
2022-03-21 00:02:21 -0400

    hdata

--- errorlog.php
+++ errorlog.php
@@ -20,9 +20,9 @@ require_once('rabbitMQLib.inc');
 error_reporting(E_ALL);
 ini_set('display_errors', 'Off');
 ini_set('log_errors', 'On');
-ini_set('error_log',"/home/parallels/git/IT490F1/IT490F1/errorlog.txt");
+ini_set('error_log',"errorlog.txt");
 $displayErrors = ini_get('display_errors');
-$errorLog = ini_get('error_log',"/home/parallels/git/IT490F1/IT490F1/errorlog.txt");
+$errorLog = ini_get('error_log',"errorlog.txt");
 $errorMessage('This is an error');
 
 //this is what reads the local files
@@ -40,7 +40,7 @@ function our_errors($errorMessage){
         fwrite($errorLog, $errorMessage);
 	fclose($errorLog);
 }
-file_put_contents ("/home/parallels/git/IT490F1/IT490F1/errorlog.txt", FILE_APPEND);               
+file_put_contents ("errorlog.txt", FILE_APPEND);               
 
 
 ?>
