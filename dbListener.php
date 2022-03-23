2a746c56ac16d0640a96d9d224b22a4526a1b73a
sarahthomas13 <st665@.njit.edu>
2022-03-23 19:33:50 -0400

    changes to listener

--- dbListener.php
+++ dbListener.php
@@ -1,52 +1,40 @@
 <?php 
 
 error_reporting(E_ALL);
-ini_set('display_errors', 'on');
-
-// setting error logging to be active
+ini_set('display_errors', 'Off');
 ini_set('log_errors', 'On');
-ini_set('error_log', dirname(__FILE__).'/home/parallels/git/IT490F1/IT490F1/errorlog.txt');
-
-// PHP code for logging error into a given file
-// error message to be logged
-$error_message = "This is an error message!";
-
-// path of the log file where errors need to be logged
-$log_file = "'/home/parallels/git/IT490F1/IT490F1/errorlog.txt'";
-
-
-
-// setting the logging file in php.ini
-ini_set('error_log', $log_file);
-
-// logging the error
-error_log($error_message);
-
-errorReporting();
+ini_set('error_log', '/home/parallels/IT490F1/IT490F1/errorlog.txt');
 
 //Requried files
 require_once('/home/parallels/git/IT490F1/IT490F1/path.inc');
 require_once('/home/parallels/git/IT490F1/IT490F1/get_host_info.inc');
 require_once('/home/parallels/git/IT490F1/IT490F1/rabbitMQLib.inc');
-require_once('dbFunctions.php');
+require_once('/home/parallels/git/IT490F1/IT490F1/errorlog.php');
 
 function requestProcessor($request){
         echo "received request".PHP_EOL;
         echo $request['type'];
         var_dump($request);
         
-        if(!isset($request['type'])){
+	if(!isset($request['type']))
+	{
             return array('message'=>"ERROR: Message type is not supported");
 	}
 
-	$response_msg = file_get_contents('errorlog.txt', $request['error_string'], FILE_APPEND);
-	$dist_msg = file_get_contents('errorlog.txt');
-	echo "outcome:" .$response_msg;
-	break;
+	switch($request['type'])
+	{
+	case "parallels":
+		echo "Errors";
+		$response_msg = file_get_contents('errorlog.txt', $request['error_string'], FILE_APPEND);
+		
+	}
+
+	echo $response_msg;
+	return $response_msg;
 
 }
 
-$server = new RabbitMQServer('log1.ini');
+$server = new RabbitMQServer('log1.ini', 'testserver');
 echo "Error in Database Server START\N";
 $server->process_requests('requestProcessor');
 
